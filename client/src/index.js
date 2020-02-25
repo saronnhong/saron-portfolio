16c2d532d9ea7418741c46e75b1c8ea38cf56a1e
Saron <saronnhong@gmail.com>
2020-02-24 20:30:50 -0800

    copy compoments over

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+// import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Contact from './pages/Contact';
