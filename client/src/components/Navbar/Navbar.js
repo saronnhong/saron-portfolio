8878a417cbe3eef9b4ee1991fe5fde52d23722a4
Saron <saronnhong@gmail.com>
2020-03-10 15:43:51 -0700

    add highlights to current links on navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import { withRouter } from 'react-router-dom';
 import "./style.css";
 
-function Navbar(props) {
-    return (
-        <div className="navbar-fixed">
-            <nav id="navBar" className={props.backgroundColor}>
-                <div className="nav-wrapper nav-background">
-                    <div className="container">
-                        <a href="/Home" className="brand-logo left">M&A INC.<i class="large material-icons whiteIcon">wifi_tethering</i></a>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a href="/home">Home</a></li>
-                            <li><a href="/about">About</a></li>
-                            <li><a href="/service">Services</a></li>
-                            <li><a id="contactLink" href="/contact">Contact</a></li>
-                        </ul>
+class Navbar extends React.Component {
+    render() {
+        let isActive = this.props.location.pathname;
+        return (
+            <div className="navbar-fixed">
+                <nav id="navBar" >
+                    <div className="nav-wrapper nav-background">
+                        <div className="container">
+                            <Link smooth to="/home">
+                                <a href="/Home" className="brand-logo left">M&A INC.<i class="large material-icons whiteIcon">wifi_tethering</i></a>
+                            </Link>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li>
+                                    <Link smooth to="/home">
+                                        <a className={isActive === "/home" ? "activeLink navLinks" : "navLinks"}>Home</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link smooth to="/about">
+                                        <a className={isActive === "/about" ? "activeLink navLinks" : "navLinks"}>About</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link smooth to="/service">
+                                        <a className={isActive === "/service" ? "activeLink navLinks" : "navLinks"}>Services</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link smooth to="/contact">
+                                        <a className={isActive === "/contact" ? "activeLink navActiveContact" : "contactLink"}>Contact</a>
+                                    </Link>
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-    );
+                </nav>
+            </div>
+        );
+    }
 }
 
-export default Navbar;
+export default withRouter(Navbar);
