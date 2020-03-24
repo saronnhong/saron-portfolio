67722773ce57c475c7b4e60dfcae37b112e43adb
Saron <saronnhong@gmail.com>
2020-03-24 16:17:50 -0700

    add services page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,9 +10,9 @@ class Home extends Component {
             <div className="home">
                 <div className="homeTop">
                     <div className="container">
-                        <div className="row imageContainer">
-                            <h1 className="homeHeader">Survivable Space Systems</h1>
-                            <p className="homeText">Engineering Services and Technology Development Get all your meetings minutes, tasks and decisions in one place. Share them with anyone instantly. Export them to all the tools you already use.</p>
+                        <div className="row bannerContainer">
+                            <h1 className="homeBanner">Survivable Space Systems</h1>
+                            <p className="homeBannerText">Engineering Services and Technology Development Get all your meetings minutes, tasks and decisions in one place. Share them with anyone instantly. Export them to all the tools you already use.</p>
                         </div>
                         <button type="button" class="btn btn-dark contactUsBtn">Contact Us</button>
                     </div>
@@ -23,7 +23,7 @@ class Home extends Component {
                             <h1 className="homeTitle">
                                 Milanowski. Your Mission is Ours.
                             </h1>
-                            <p className="homeContent">
+                            <p className="homeText">
                                 Every day, our 110,000 employees come to work with one focus – our customers’ missions. Whether it's protecting citizens or advancing the boundaries of science, these missions are some of the most important and challenging in the world. We bring an unwavering commitment to help our customers succeed, and it’s that sense of purpose and opportunity to make a difference in the world that drives us every day.
                             </p>
                         </div>
--- client/src/pages/Service/Service.js
+++ client/src/pages/Service/Service.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./style.css";
-import Modal from "../../components/Modal"
 
 function Service() {
     return (
-        <div>
-            <div className="row">
+        <div className="service">
+            {/* <div className="row">
                 <div className="aboutUsTitle">
                     <h1>Services</h1>
                 </div>
@@ -29,7 +28,7 @@ function Service() {
                         <p className="buttonTitle">Radiation Hardening by Process</p>
                         <p className="buttonText">& Design for ASICs & FPGAs</p>
                         <br></br>
-                        {/* <a className="waves-effect waves-light btn-large green">More Info</a> */}
+                        
                         <Modal id="1" header="Radiation Hardening by Process & Design for ASICs & FPGAs" content="Dramatic increases in sensor resolution in remote-sensing space payloads are causing a processing bottleneck, as downlink bandwidth is not keeping pace. Operators require onboard processing so that satellites send processed information, not just raw data. It is a growing challenge for the roughly 100 remote sensing satellites launched each year, each carrying as many as eight payload instruments. Flash-based field-programmable gate array (FPGA) technology is now being applied to the problem, combining high-speed signal processing with special built-in radiation mitigation techniques to keep systems operational in harsh radiation environments." />
                     </button>
                     <button className="col s4 serviceBox2" >
@@ -37,7 +36,6 @@ function Service() {
                         <p className="buttonTitle">Commercial Off The Shelf (COTS)</p>
                         <p className="buttonText">part assessment/grading for use in radiation environments</p>
                         <br></br>
-                        {/* <a className="waves-effect waves-light btn-large green">More Info</a> */}
                         <Modal id="2" header="Radiation Hardening by Process & Design for ASICs & FPGAs" content="Dramatic increases in sensor resolution in remote-sensing space payloads are causing a processing bottleneck, as downlink bandwidth is not keeping pace. Operators require onboard processing so that satellites send processed information, not just raw data. It is a growing challenge for the roughly 100 remote sensing satellites launched each year, each carrying as many as eight payload instruments. Flash-based field-programmable gate array (FPGA) technology is now being applied to the problem, combining high-speed signal processing with special built-in radiation mitigation techniques to keep systems operational in harsh radiation environments." />
                     </button>
                     <button className="col s4 serviceBox" >
@@ -45,7 +43,6 @@ function Service() {
                         <p className="buttonTitle">Technology Computer Aided Design (TCAD)</p>
                         <p className="buttonText">Simulations & Design of Experiments</p>
                         <br></br>
-                        {/* <a className="waves-effect waves-light btn-large green">More Info</a> */}
                         <Modal id="1" header="Radiation Hardening by Process & Design for ASICs & FPGAs" content="Dramatic increases in sensor resolution in remote-sensing space payloads are causing a processing bottleneck, as downlink bandwidth is not keeping pace. Operators require onboard processing so that satellites send processed information, not just raw data. It is a growing challenge for the roughly 100 remote sensing satellites launched each year, each carrying as many as eight payload instruments. Flash-based field-programmable gate array (FPGA) technology is now being applied to the problem, combining high-speed signal processing with special built-in radiation mitigation techniques to keep systems operational in harsh radiation environments." />
                     </button>
 
@@ -100,6 +97,59 @@ function Service() {
                         <br></br>
                         <a className="waves-effect waves-light btn-large green">More Info</a>
                     </button>
+                </div>
+            </div> */}
+            <div className="serviceBanner">
+                <h1 className="whatWeDo">
+                    What We Do
+                </h1>
+            </div>
+            <div className="serviceContent container">
+                <h1 className="serviceTitle text-center">
+                    Services
+                </h1>
+                <p className="serviceText">
+                    Every day, our 110,000 employees come to work with one focus – our customers’ missions. Whether it's protecting citizens or advancing the boundaries of science, these missions are some of the most important and challenging in the world. We bring an unwavering commitment to help our customers succeed, and it’s that sense of purpose and opportunity to make a difference in the world that drives us every day.
+                </p>
+                <div className="row serviceRow">
+                    <div className= "col-md-4">
+                        <img className="serviceListImage" src="/images/raisting-3930778_640.jpg" width="95%" alt="service_image"  />
+                        <h2 className="serviceListTitle">Radiation Hardening by Process and Design for ASICs & FPGAs</h2>
+                        <p className="serviceListText">Protecting the U.S., its friends and allies has never been more urgent. Our missile-defense systems aim to put the best technology up against the world's most pressing threats.</p>
+                        <button type="button" class="btn btn-outline-info">Learn More</button>
+                    </div>
+                    <div className= "col-md-4">
+                        <img className="serviceListImage" src="/images/rocket-launch-67721_640.jpg" width="95%" alt="service_image"  />
+                        <h2 className="serviceListTitle">Electronic Design Automation (EDA) Flows</h2>
+                        <p className="serviceListText">Protecting the U.S., its friends and allies has never been more urgent. Our missile-defense systems aim to put the best technology up against the world's most pressing threats.</p>
+                        <button type="button" class="btn btn-outline-info">Learn More</button>
+                    </div>
+                    <div className= "col-md-4">
+                        <img className="serviceListImage" src="/images/space-station-60615_640.jpg" width="95%" alt="service_image"  />
+                        <h2 className="serviceListTitle">Radiation Testing and Component Analyses</h2>
+                        <p className="serviceListText">Protecting the U.S., its friends and allies has never been more urgent. Our missile-defense systems aim to put the best technology up against the world's most pressing threats.</p>
+                        <button type="button" class="btn btn-outline-info">Learn More</button>
+                    </div>
+                </div>
+                <div className="row serviceRow">
+                    <div className= "col-md-4">
+                        <img className="serviceListImage" src="/images/raisting-3930778_640.jpg" width="95%" alt="service_image"  />
+                        <h2 className="serviceListTitle">Radiation Hardening by Process and Design for ASICs & FPGAs</h2>
+                        <p className="serviceListText">Protecting the U.S., its friends and allies has never been more urgent. Our missile-defense systems aim to put the best technology up against the world's most pressing threats.</p>
+                        <button type="button" class="btn btn-outline-info">Learn More</button>
+                    </div>
+                    <div className= "col-md-4">
+                        <img className="serviceListImage" src="/images/rocket-launch-67721_640.jpg" width="95%" alt="service_image"  />
+                        <h2 className="serviceListTitle">Electronic Design Automation (EDA) Flows</h2>
+                        <p className="serviceListText">Protecting the U.S., its friends and allies has never been more urgent. Our missile-defense systems aim to put the best technology up against the world's most pressing threats.</p>
+                        <button type="button" class="btn btn-outline-info">Learn More</button>
+                    </div>
+                    <div className= "col-md-4">
+                        <img className="serviceListImage" src="/images/space-station-60615_640.jpg" width="95%" alt="service_image"  />
+                        <h2 className="serviceListTitle">Radiation Testing and Component Analyses</h2>
+                        <p className="serviceListText">Protecting the U.S., its friends and allies has never been more urgent. Our missile-defense systems aim to put the best technology up against the world's most pressing threats.</p>
+                        <button type="button" class="btn btn-outline-info">Learn More</button>
+                    </div>
                 </div>
             </div>
         </div>
