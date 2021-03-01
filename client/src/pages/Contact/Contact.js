bf26169868f705a10b3ba9fe1caee15957ee59fc
Saron <saronnhong@gmail.com>
2021-03-01 11:49:27 -0800

    add copy to clipboard of contact details

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -41,52 +41,61 @@ class Contact extends Component {
 
         return (
             <div className="contact">
-                {/* <div> */}
-                    <div className="contactPage container">
-                        <h1 className="contactTitle" >Let’s get in touch.</h1>
-                        {/* <p className="contactText">Longing to have a truly unique and memorable web app to call your own? I love exploring fresh artistic directions, experimenting with new technologies, and collaborating with others. So what are you waiting for? Let’s make it happen!</p> */}
-                        <p className="contactText">I am always open to discussing development work or collaborative opportunities. So what are you waiting for? Let’s make it happen!</p>
-                        <div className="row contactContent">
-                            <div className="col-md-4">
-                                <h1 className="ourOffice">Contact</h1>
-                                <div className="contactInfo">
-                                    <p><a className="contactDetails" href="tel:209-598-4517"><i className="fa fa-phone"></i> 209.598.4517</a></p>
-                                    <p><a className="contactDetails" href="mailto:saronnhong@gmail.com"><i className="fa fa-envelope"></i> saronnhong@gmail.com</a></p>
-                                    <p><a className="contactDetails" href="https://linkedin.com/in/saronnhong" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i> linkedin.com/in/saronnhong</a></p>
-                                    <p><a className="contactDetails" href="https://lgithub.com/saronnhong" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i> github.com/saronnhong</a></p>
-                                </div>
-                            </div>
-                            <div className="col-md-8">
-                                <h1 className="contactUs">Email</h1>
-                                <p id="confirmMessage">{email.confirmMsg}</p>
-                                <form>
-                                    <p>
-                                        <input className="textFields" placeholder="Name" value={email.subject}
-                                            onChange={e => this.setState({ email: { ...email, subject: e.target.value } })} />
-                                    </p>
-                                    <p>
-                                        <input className="textFields" placeholder="Phone" value={email.phone}
-                                            onChange={e => this.setState({ email: { ...email, phone: e.target.value } })} />
-                                    </p>
-                                    <p>
-                                        <input className="textFields" placeholder="Email Address" value={email.from}
-                                            onChange={e => this.setState({ email: { ...email, from: e.target.value } })} />
-                                    </p>
-                                    <div className="input-group full">
-                                        <div className="input-group-prepend">
-                                            <span className="input-group-text">Message</span>
-                                        </div>
-                                        <textarea className="form-control" rows="7" aria-label="With textarea" value={email.text}
-                                            onChange={e => this.setState({ email: { ...email, text: e.target.value } })}></textarea>
-                                    </div>
-                                    <button className="btn btn-dark emailSubmitBtn hvr-shadow hvr-sweep-to-right-sendbtn" type="button" onClick={() => {
-                                        this.sendEmail();
-                                    }}> Send Message</button>
-                                </form>
+                <div className="contactPage container">
+                    <h1 className="contactTitle" >Let’s get in touch.</h1>
+                    <p className="contactText">I am always open to discussing development work or collaborative opportunities. So what are you waiting for? Let’s make it happen!</p>
+                    <div className="row contactContent">
+                        <div className="col-md-4">
+                            <h1 className="ourOffice">Contact</h1>
+                            <div className="contactInfo">
+                                <p>
+                                    <i className="fa fa-phone" onClick={() => { navigator.clipboard.writeText("2095984517") }}></i>
+                                    <a className="contactDetails" href="tel:209-598-4517"> 209.598.4517</a>
+                                </p>
+                                <p>
+                                    <i className="fa fa-envelope" onClick={() => { navigator.clipboard.writeText("saronnhong@gmail.com") }}></i>
+                                    <a className="contactDetails" href="mailto:saronnhong@gmail.com"> saronnhong@gmail.com</a>
+                                </p>
+                                <p>
+                                    <i class="fab fa-linkedin" onClick={() => { navigator.clipboard.writeText("linkedin.com/in/saronnhong") }}></i>
+                                    <a className="contactDetails" href="https://linkedin.com/in/saronnhong" target="_blank" rel="noopener noreferrer"> linkedin.com/in/saronnhong</a>
+                                </p>
+                                <p>
+                                    <i class="fab fa-github" onClick={() => { navigator.clipboard.writeText("github.com/saronnhong") }}></i>
+                                    <a className="contactDetails" href="https://github.com/saronnhong" target="_blank" rel="noopener noreferrer"> github.com/saronnhong</a>
+                                </p>
                             </div>
                         </div>
+                        <div className="col-md-8">
+                            <h1 className="contactUs">Email</h1>
+                            <p id="confirmMessage">{email.confirmMsg}</p>
+                            <form>
+                                <p>
+                                    <input className="textFields" placeholder="Name" value={email.subject}
+                                        onChange={e => this.setState({ email: { ...email, subject: e.target.value } })} />
+                                </p>
+                                <p>
+                                    <input className="textFields" placeholder="Phone" value={email.phone}
+                                        onChange={e => this.setState({ email: { ...email, phone: e.target.value } })} />
+                                </p>
+                                <p>
+                                    <input className="textFields" placeholder="Email Address" value={email.from}
+                                        onChange={e => this.setState({ email: { ...email, from: e.target.value } })} />
+                                </p>
+                                <div className="input-group full">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">Message</span>
+                                    </div>
+                                    <textarea className="form-control" rows="7" aria-label="With textarea" value={email.text}
+                                        onChange={e => this.setState({ email: { ...email, text: e.target.value } })}></textarea>
+                                </div>
+                                <button className="btn btn-dark emailSubmitBtn hvr-shadow hvr-sweep-to-right-sendbtn" type="button" onClick={() => {
+                                    this.sendEmail();
+                                }}> Send Message</button>
+                            </form>
+                        </div>
                     </div>
-                {/* </div> */}
+                </div>
             </div>
         );
     }
